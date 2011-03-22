f2fc8b31b69de3364f457d6967ea2ba84ca1f53f
Florent Jaby <florent.jaby@gmail.com>
2011-03-22 18:48:58 +0100

    commented and removed useless output

--- bin/app.js
+++ bin/app.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// if you're looking for an entry point, begin reading at line 130
+// if you're looking for an entry point, begin reading at line 90
 
 // requires
 var http = require('http');
@@ -7,9 +7,8 @@ var qs = require('querystring');
 var url = require('url');
 
 function fetch (data, callback) {
-	var url = meme_generator.url;
-	var post = qs.stringify(data);
-	console.log('dump post: %s', post);
+	var url = meme_generator.url; // this is JSON object
+	var post = qs.stringify(data); // stringify into urlencoded
 	var options = {
 		host: url.hostname,
 		port: url.port || 80,
@@ -17,11 +16,12 @@ function fetch (data, callback) {
 		method: 'POST',
 	};
 	options.headers = {
+		// do NOT forget this
 		'Content-Type': 'application/x-www-form-urlencoded'
 	};
-	console.log('dump options', options);
 	var req = http.request(options);
 	req.on('error', function(error) {
+		// send all errors to the global handler
 		return callback(error);
 	});
 	req.on('response', function(res) {
@@ -32,7 +32,10 @@ function fetch (data, callback) {
 		if(!res.headers.location) {
 			return callback(new Error("No location header"));
 		}
-		callback(null, url.host+res.headers.location+'.jpg');
+		// we couldn't care less about the data
+		// thanks to the fine url rewriting rules at memegenerator
+		// we can assume that the image url is this
+		callback(null, 'http://'+url.host+res.headers.location+'.jpg');
 	});
 	req.end(post+'\n');
 }
@@ -47,13 +50,15 @@ function simple (id, template_name, first_line) {
 	// return a closured function that fetches the link
 	// page and looks for the link
 	var res = function(line1, line2) {
-		var cb = [].pop.call(arguments);
+		var cb = [].pop.call(arguments); // arguments is no real array
 		// construct the data to post
 		var data = {
 			templateType: 'AdviceDogSpinoff',
 			templateID: id,
 			generatorName: template_name
 		};
+		// add the default first line as first argument
+		// TODO: improve this
 		if(first_line) [].unshift.call(arguments, first_line);
 		for (var i = 0; i < arguments.length; ++i) {
 			data['text'+i] = arguments[i];
@@ -73,7 +78,10 @@ var meme_generator = {
 	version: '0.1.0',
 	url: url.parse('http://memegenerator.net/Instance/CreateOrEdit'),
 	//url: url.parse('http://localhost:3000/Instance/CreateOrEdit'),
+	
 	// I could do the trick with the user agent thing. CAN HAZ MOTIVATION?
+
+	// here comes the ugly inline data
 	memes: {
 		ANTEATER: simple(41191, 'Anteater'),
 		V_BABY: simple(11140, 'Victory-Baby')
@@ -83,11 +91,13 @@ var meme_generator = {
 // beginning program
 // parse args and then do things
 
+// print usage when no arguments are provided
 if(process.argv.length < 3) {
 	console.log("meme [GENERATOR|--list] LINE [ADDITIONAL_LINES]");
 	process.exit();
 }
 
+// list available memes when asked for it
 if(process.argv[2] === '--list') {
 	for(var key in meme_generator.memes) {
 		console.log("%s: %s", key, meme_generator.memes[key].template_name)
@@ -97,22 +107,24 @@ if(process.argv[2] === '--list') {
 
 // that was the easy part
 
-var symbol = process.argv[2];
+var symbol = process.argv[2]; // the DEFINE-style name of the meme -> V_BABY
 var text = process.argv.slice(3);
 if(!(symbol in meme_generator.memes)) {
+	// haha! you thought I was that dumb ?
 	console.error('unknown meme %s',symbol);
 	console.error('use --list to see what is available');
 	process.exit(1);
 } 
 
+// add the callback as last arguments see last line
 text.push(function callback(err, link) {
-	console.log('in master callback')
 	if(err) {
 		throw(err);
-		process.exit(2);
+		process.exit(2); // useless as long as we keep throwing errors
 	}
 	console.log(link);
 })
 
 // this has to be the most complicated line of all this
+// basically, find the function associated with a meme and call it
 meme_generator.memes[symbol].apply(null, text);
