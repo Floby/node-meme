c1af92592d59ddbbe5b4e0e11dd336a0279f28c6
Florent Jaby <florent.jaby@gmail.com>
2011-03-22 16:48:13 +0100

    Oopsie dootsie, coded everything in one hit
    may not work, expect further commits

--- bin/app.js
+++ bin/app.js
@@ -1,9 +1,39 @@
 #!/usr/bin/env node
+// if you're looking for an entry point, begin reading at line 130
 
 // requires
 var http = require('http');
+var qs = require('querystring');
+var url = require('url');
 
-
+function fetch (data, callback) {
+	var url = meme_generator.url;
+	var post = qs.stringify(data);
+	console.log('dump post: %s', post);
+	var req = http.request({
+		host: url.host,
+		port: url.port || 80,
+		path: url.pathname,
+		method: 'POST',
+	});
+	req.on('error', function(error) {
+		return callback(error);
+	});
+	req.on('response', function(res) {
+		if(res.statusCode !== 302) {
+			res.end();
+			return callback(new Error("Got error "+res.statusCode));
+		}
+		if(!res.headers.location) {
+			res.end();
+			return callback(new Error("No location header"));
+		}
+		callback(null, url.host+res.headers.location+'.jpg');
+	});
+	req.write(post);
+	req.end();
+	console.log('dumping req', req);
+}
 
 
 /**
@@ -12,7 +42,21 @@ var http = require('http');
  * two lines of text only
  */
 function simple (id, template_name, first_line) {
-	var res = function() {
+	// return a closured function that fetches the link
+	// page and looks for the link
+	var res = function(line1, line2) {
+		var cb = [].pop.call(arguments);
+		// construct the data to post
+		var data = {
+			templateId: id,
+			generatorName: template_name,
+			templateType: 'AdviceDogSpinoff'
+		};
+		if(first_line) [].unshift.call(arguments, first_line);
+		for (var i = 0; i < arguments.length; ++i) {
+			data['text'+i] = arguments[i];
+		};
+		fetch(data, cb);
 	}
 	res.id = id;
 	res.template_name = template_name;
@@ -25,10 +69,11 @@ function simple (id, template_name, first_line) {
  */
 var meme_generator = {
 	version: '0.1.0',
+	url: url.parse('http://memegenerator.net/Instance/CreateOrEdit'),
 	// I could do the trick with the user agent thing. CAN HAZ MOTIVATION?
 	memes: {
 		ANTEATER: simple(41191, 'Anteater'),
-		V_BABY: simple(11140, 'Victory Baby')
+		V_BABY: simple(11140, 'Victory-Baby')
 	}
 }
 
@@ -53,13 +98,18 @@ if(process.argv[2] === '--list') {
 var symbol = process.argv[2];
 var text = process.argv.slice(3);
 if(!(symbol in meme_generator.memes)) {
-	console.log('unknown meme %s',symbol);
-	console.log('use --list to see what is available');
+	console.error('unknown meme %s',symbol);
+	console.error('use --list to see what is available');
 	process.exit(1);
 } 
 
-text.push(function callback(link) {
+text.push(function callback(err, link) {
+	if(err) {
+		console.error(err);
+		process.exit(2);
+	}
 	console.log(link);
 })
 
+// this has to be the most complicated line of all this
 meme_generator.memes[symbol].apply(null, text);
